0331cd94c62a9659e7b96c5a483001be8b8dd231
Sebastian Ruiz <imsebarz@gmail.com>
2022-03-07 08:54:45 -0500

    created dashboard

---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,73 @@
+import { Col, Container, Row } from "react-bootstrap"
+import './styles.scss'
+import EthIcon from './assets/eths.svg'
+import ClockIcon from './assets/Vector.svg'
+import CardsIcon from './assets/Vector-1.svg'
+import Slider from './Components/Slider'
+const Dashboard = () => {
+    return (
+        <>
+            <Container className="mt-5">
+                <Container className="w-75 mt-5 mb-5">
+                    <Row>
+                        <Col className="shadow-sm justify-content-center p-5 bg-white mt-3 mb-3 mw-75" id='home-stats'>
+                            <Row>
+                                <h3 className="text-center pb-3">About You</h3>
+                            </Row>
+                            <Row className="justify-content-between gap-2 h-25 text-white">
+                                <Col className="info d-flex flex-row bg-primary align-items-center  flex-grow-0">
+                                    <Col className="w-auto flex-grow-0 ">
+                                        <img
+                                            src={ClockIcon}
+                                            width="60"
+                                            height="60"
+                                            className="d-inline-block align-top w-auto m-2"
+                                            alt=""
+                                        />
+                                    </Col>
+                                    <Col className='p-3'>
+                                        <Row className='fw-bold fs-3 justify-content-center'>01-01-2022</Row>
+                                        <Row className="justify-content-center">last seen</Row>
+                                    </Col>
+                                </Col>
+                                <Col className="info cards d-flex flex-row align-items-center flex-grow-0">
+                                    <Col className="w-auto flex-grow-0 p-1">
+                                        <img
+                                            src={CardsIcon}
+                                            width="60"
+                                            height="60"
+                                            className="d-inline-block align-top w-auto m-2"
+                                            alt=""
+                                        />
+                                    </Col>
+                                    <Col className='p-3'>
+                                        <Row className='fw-bold fs-3 justify-content-center'>22</Row>
+                                        <Row className="justify-content-center">Cards</Row>
+                                    </Col>
+                                </Col>
+                                <Col className="info d-flex flex-row bg-success align-items-center flex-grow-0">
+                                    <Col className="w-auto flex-grow-0">
+                                        <img
+                                            src={EthIcon}
+                                            width="60"
+                                            height="60"
+                                            className="d-inline-block align-top w-auto m-2"
+                                            alt=""
+                                        />
+                                    </Col>
+                                    <Col className='p-3'>
+                                        <Row className='fw-bold fs-3 justify-content-center flex-grow-0'>0.0023</Row>
+                                        <Row className="justify-content-center">Eth's</Row>
+                                    </Col>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Container>
+            </Container>
+            <Slider></Slider>
+        </>
+    )
+}
+
+export default Dashboard
